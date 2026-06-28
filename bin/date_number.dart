d177ee802e739a7209fa29fc64a02e2a2239d3c6
unknown <faujanadim4@gmail.com>
2026-06-28 11:39:52 +0800

    tipe data number

---
+++ bin/date_number.dart
@@ -0,0 +1,16 @@
+void main() {
+  int umur =
+      20; //int tipe data untuk menyimpan bilangan bulat, mendeklarasikan variabel umur dengan tipe data int dan menginisialisasi nilainya dengan 20
+  print(umur);
+
+  double tinggi =
+      1.75; //double tipe data untuk menyimpan bilangan desimal, mendeklarasikan variabel tinggi dengan tipe data double dan menginisialisasi nilainya dengan 1.75
+  print(tinggi);
+
+  num berat =
+      60.5; //num tipe data untuk menyimpan bilangan bulat atau desimal, mendeklarasikan variabel berat dengan tipe data num dan menginisialisasi nilainya dengan 60.5
+  print(berat);
+
+  berat = 70; //mengubah nilai variabel berat menjadi 70
+  print(berat);
+}
